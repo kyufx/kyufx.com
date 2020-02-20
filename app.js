003601ea35ac62c28878269b3793e638e06d9836
kyufx <61257615+kyufx@users.noreply.github.com>
2020-02-19 20:21:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+
+  burger.addEventListener("", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+navSlide();
